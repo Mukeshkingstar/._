ed7d6f14aa22b66cf93f7cc2e1ac9339032b8035
Mukesh prajapati <94641465+Mukeshkingstar@users.noreply.github.com>
2022-01-26 00:40:24 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,15 @@
+<?php    
+include 'conn.php';
+
+if(isset($_POST['button'])){
+$name = $_POST['mobile_number'];
+
+$sql = "INSERT INTO contactform VALUES ('$name')";
+$res = mysqli_query($con,$sql);
+if ($res) {
+      unset($_POST);
+      echo "New record created successfully";
+      header("Location:http://localhost/contactform");
+}
+}
+?>
